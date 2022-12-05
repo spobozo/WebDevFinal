4c1f3f245a60f127784dd4b27f50287e607074c3
Mason Williamson <56938029+mwilliamson023@users.noreply.github.com>
2022-12-04 18:30:53 -0700

    updated submit button

--- client/src/components/ManageEmail.jsx
+++ client/src/components/ManageEmail.jsx
@@ -3,9 +3,15 @@ import React from 'react';
 const ManageEmail = () => {
 
   return ( 
+
     <button className="submitBtn"
           onClick={() => {
+            const confirmBox = window.confirm(
+              "Are you sure you want to send this email?"
+            )
+            if (confirmBox === true) {
               alert("email sent")
+            }
           }}
         >Send Email</button>
   )
