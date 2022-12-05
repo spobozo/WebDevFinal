e122b8dcb06a3181e2a4fbcf3748e5f9c77306ea
Mason Williamson <56938029+mwilliamson023@users.noreply.github.com>
2022-12-05 15:05:15 -0700

    Added ManageEmail.jsx content
    No Css, no functionality, just bare-bones html content

--- client/src/components/ManageEmail.jsx
+++ client/src/components/ManageEmail.jsx
@@ -4,7 +4,40 @@ const ManageEmail = () => {
 
   return ( 
 
-    <button className="submitBtn"
+    <div>
+      <h2>New Email</h2>
+      <label>Send to:</label>
+      <div>
+        <input id="customEmail" type="text" name="email" />
+        <button className="OptionBtn">All</button>
+        <button className="OptionBtn">Scheduled Volunteers</button>
+      </div>
+      <label>Subject:</label>
+      <div>
+        <input id="customSubject" type="text" name="subject" />
+      </div>
+      <label>Body:</label>
+      <div>
+        <button className="OptionBtn">Voucher Template</button>
+        <button className="OptionBtn">Cancellation Template</button>
+        <button className="OptionBtn">Waiver Template</button>
+      </div>
+      <label>Custom:</label>
+      <div>
+        <input id="customBody" type="text" name="body" />
+      </div>
+      <div>
+        <button className="clearBtn"
+        onClick={() => {
+          const confirmBox = window.confirm(
+            "Are you sure you want to clear all fields?"
+          )
+          if(confirmBox === true) {
+            alert("fields cleared")
+          }
+        }}
+        >Clear</button>
+        <button className="sendBtn"
           onClick={() => {
             const confirmBox = window.confirm(
               "Are you sure you want to send this email?"
@@ -14,6 +47,8 @@ const ManageEmail = () => {
             }
           }}
         >Send Email</button>
+        </div>
+    </div>
   )
 }
 
