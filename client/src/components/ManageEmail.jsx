d5134dba66ef8b48b2994dc62d81ce4e0d6b5252
Mason Williamson <56938029+mwilliamson023@users.noreply.github.com>
2022-12-11 19:11:39 -0700

    functionality aded

--- client/src/components/ManageEmail.jsx
+++ client/src/components/ManageEmail.jsx
@@ -1,55 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './ManageEmail.css';
 
-const ManageEmail = () => {
+const ManageEmail = (props) => {
+
+  const [recipientValue, setRecipientValue] = useState('')
+  const [subjectValue, setSubjectValue] = useState('')
+  const [bodyValue, setBodyValue] = useState('')
+
+  const handleRecipientChange = (event) => {
+    setRecipientValue(event.target.value)
+  }
+  const handleSubjectChange = (event) => {
+    setSubjectValue(event.target.value)
+  }
+  const handleBodyChange = (event) => {
+    setBodyValue(event.target.value)
+  }
+
+
+  const [button1,setButton1] = useState(false);
+  const [button2,setButton2] = useState(true);
+
+  const handleButton1Click = () => {
+    setButton1(true);
+    setButton2(false);
+  }
+
+  const handleButton2Click = () => {
+    setButton1(false);
+    setButton2(true);
+  }
 
   return ( 
 
-    <div>
+      <div>
       <h2>New Email</h2>
       <label>Send to:</label>
       <div>
-        <input id="customEmail" type="text" name="email" />
-        <button className="OptionBtn">All</button>
-        <button className="OptionBtn">Scheduled Volunteers</button>
-      </div>
-      <label>Subject:</label>
-      <div>
-        <input id="customSubject" type="text" name="subject" />
-      </div>
-      <label>Body:</label>
-      <div>
+        <input type="text" value={recipientValue} onChange={handleRecipientChange} />
+        <button onClick={handleButton1Click} className={button1 ? 'on' : 'off'}>All</button>
+        <button onClick={handleButton2Click} className={button2 ? 'on' : 'off'}>Scheduled Volunteers</button>
+      </div><label>Subject:</label><div>
+        <input type="text" value={subjectValue} onChange={handleSubjectChange} />
+      </div><label>Body:</label><div>
         <button className="OptionBtn">Voucher Template</button>
         <button className="OptionBtn">Cancellation Template</button>
         <button className="OptionBtn">Waiver Template</button>
-      </div>
-      <label>Custom:</label>
-      <div>
-        <input id="customBody" type="text" name="body" />
-      </div>
-      <div>
+      </div><label>Custom:</label><div>
+        <input type="text" value={bodyValue} onChange={handleBodyChange} />
+      </div><div>
         <button className="clearBtn"
-        onClick={() => {
-          const confirmBox = window.confirm(
-            "Are you sure you want to clear all fields?"
-          )
-          if(confirmBox === true) {
-            alert("fields cleared")
-          }
-        }}
+          onClick={() => {
+            const confirmBox = window.confirm(
+              "Are you sure you want to clear all fields?"
+            );
+            if (confirmBox === true) {
+              setRecipientValue("");
+              setSubjectValue("");
+              setBodyValue("");
+            }
+          } }
         >Clear</button>
         <button className="sendBtn"
           onClick={() => {
             const confirmBox = window.confirm(
               "Are you sure you want to send this email?"
-            )
+            );
             if (confirmBox === true) {
-              alert("email sent")
+              alert("email sent");
             }
-          }}
+          } }
         >Send Email</button>
-        </div>
+      </div>
     </div>
   )
 }
-
 export default ManageEmail;
