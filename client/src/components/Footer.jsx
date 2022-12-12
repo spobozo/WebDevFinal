b6dcf57933fe89d92d570a7418f16b4be026b799
spobozo <89806426+spobozo@users.noreply.github.com>
2022-12-12 11:48:18 -0700

    Create Footer.jsx

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+const Feeter = () => {
+
+    return (
+        <div>
+            <h2>Footer</h2>
+            <p><em> Payton Morris, s31k237</em></p>
+            <p>My role in the project is to build the frontend and UI components for the Email feature:</p>
+            <ul>
+                <li>client/ManageEmail.jsx - formatting correct data/grammar for email contents</li>
+                <li>client/ManageEmail.jsx - made the prototype on adobeXD</li>
+            </ul>
+            <p>Adobe XD prototype: <a href="https://xd.adobe.com/view/f82d2082-87ed-4eeb-b412-29f4483fccf3-ad7e/?hints=off">https://xd.adobe.com/view/</a></p>
+            <p>Github: <a href="https://github.com/spobozo/BSF-starter">https://github.com/spobozo/BSF-starter</a></p>
+        </div>
+    )
+}
+
+export default Feeter
